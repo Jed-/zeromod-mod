85e4e22cee752e646d79cb61a4ace71b23ca84d9
Jed- <jed885@gmail.com>
2014-08-23 20:22:26 +0200

    parsebar fix

--- src/fpsgame/bar.h
+++ src/fpsgame/bar.h
@@ -2,6 +2,7 @@ void parsebar(const char *m, int cn) {
 	clientinfo *ci = getinfo(cn);
 	if(!_force_bot || !bots.inrange(0) || !ci || ci->state.aitype==AI_BOT) return;
 	clientinfo *b = getinfo(MAXCLIENTS);
+	if(!b) return;
 	defformatstring(bname)("%s", b->name);
 	for(int j=0; bname[j]; j++) bname[j] = tolower(bname[j]);
 	defformatstring(str0)("hi, %s", bname);
