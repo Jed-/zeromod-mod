9f4d506555d7bae5e05e5ba383f46399fb496a3a
Jed- <jed885@gmail.com>
2014-09-17 18:50:36 +0200

    managing multiple barmen

--- src/fpsgame/bar.h
+++ src/fpsgame/bar.h
@@ -21,72 +21,75 @@ void offer(int whocn, int tocn, char *what = (char*)"beer", int howmuch = 1) {
 }
 ICOMMAND(offer, "iisi", (int *whocn, int *tocn, char *what, int *howmuch), offer(*whocn, *tocn, what, *howmuch));
 void parsebar(const char *m, int cn) {
-	if(!histrings.inrange(0)) return;
+//	if(!histrings.inrange(0)) return;
 	clientinfo *ci = getinfo(cn);
 	if(!ci || ci->state.aitype==AI_BOT) return;
 	int botcn = -1;
 	loopv(bots) {
 		if(bots[i] && bots[i]->state.state==CS_SPECTATOR) {
 			botcn = bots[i]->clientnum;
-			break;
-		}
-	}
-	if(botcn==-1) return;
-	clientinfo *b = getinfo(botcn);
-	if(!b) return;
-	defformatstring(bname)("%s", b->name);
-	for(int j=0; bname[j]; j++) bname[j] = tolower(bname[j]);
-	bool ishi = false;
-	loopv(histrings) {
-		defformatstring(str1)("%s %s", histrings[i].str, bname);
-		defformatstring(str2)("%s, %s", histrings[i].str, bname);
-		if(strcasestr(m, str1) || strcasestr(m, str2)) {
-			ishi = true;
-			break;
-		}
-	}
-	defformatstring(cmd)("barhi %d %d", botcn, cn);
-	if(ishi && identexists("barhi")) execute(cmd);
+			if(botcn < 0 || botcn > 255) continue;
+			clientinfo *b = getinfo(botcn);
+			if(!b) continue;
+			defformatstring(bname)("%s", b->name);
+			for(int j=0; bname[j]; j++) bname[j] = tolower(bname[j]);
+			if(histrings.inrange(0)) {
+				bool ishi = false;
+				loopv(histrings) {
+					defformatstring(str1)("%s %s", histrings[i].str, bname);
+					defformatstring(str2)("%s, %s", histrings[i].str, bname);
+					if(strcasestr(m, str1) || strcasestr(m, str2)) {
+						ishi = true;
+						break;
+					}
+				}
+				defformatstring(cmd)("barhi %d %d", botcn, cn);
+				if(ishi && identexists("barhi")) execute(cmd);
+			}
 	
-	if(!drinks.inrange(0)) return;
-	int idx = _strcasestr((char *)m, bname);
-	if(idx <= -1) return;
-	string barcmd;
-	copystring(barcmd, (char *)&m[idx]);
-	char *argv[260];
-	int argc = _argsep(barcmd, 260, argv);
-	if(argc < 2) return;
-	if(strcasecmp(argv[0], bname) || strcasecmp(argv[1], drinkcmd)) return;
-	char list[MAXTRANS];
-	list[0] = '\0';
-	loopv(drinks) {
-		concatstring(list, drinks[i].name);
-		if(i < (drinks.length()-1)) concatstring(list, " ");
-	}
-	if(argc < 3 || !strcasecmp(argv[2], "list") || !strcasecmp(argv[2], "menu")) {
-		defformatstring(_cmd)("barlist %d %d \"%s\"", botcn, cn, list);
-		if(identexists("barlist")) execute(_cmd);
-		return;
-	}
-	bool listed = false;
-	loopv(drinks) {
-		if(!strcasecmp(drinks[i].name, argv[2])) {
-			listed = true;
-			break;
+//			if(!drinks.inrange(0)) return;
+			if(drinks.inrange(0)) {
+				int idx = _strcasestr((char *)m, bname);
+				if(idx <= -1) continue;
+				string barcmd;
+				copystring(barcmd, (char *)&m[idx]);
+				char *argv[260];
+				int argc = _argsep(barcmd, 260, argv);
+				if(argc < 2) continue;
+				if(strcasecmp(argv[0], bname) || strcasecmp(argv[1], drinkcmd)) return;
+				char list[MAXTRANS];
+				list[0] = '\0';
+				loopv(drinks) {
+					concatstring(list, drinks[i].name);
+					if(i < (drinks.length()-1)) concatstring(list, " ");
+				}
+				if(argc < 3 || !strcasecmp(argv[2], "list") || !strcasecmp(argv[2], "menu")) {
+					defformatstring(_cmd)("barlist %d %d \"%s\"", botcn, cn, list);
+					if(identexists("barlist")) execute(_cmd);
+					continue;
+				}
+				bool listed = false;
+				loopv(drinks) {
+					if(!strcasecmp(drinks[i].name, argv[2])) {
+						listed = true;
+						break;
+					}
+				}
+				if(!listed) {
+					defformatstring(_cmd)("barunlisted %d %d %s", botcn, cn, argv[2]);
+					if(identexists("barunlisted")) execute(_cmd);
+					continue;
+				}
+				int num = 1;
+				if(argc >= 4) {
+					num = atoi(argv[3]);
+				}
+				if(num || strcmp(argv[3], "0")) {
+					defformatstring(_cmd)("bardrink %d %d %s %d", botcn, cn, argv[2], num);
+					if(identexists("bardrink")) execute(_cmd);
+				}
+			}
 		}
-	}
-	if(!listed) {
-		defformatstring(_cmd)("barunlisted %d %d %s", botcn, cn, argv[2]);
-		if(identexists("barunlisted")) execute(_cmd);
-		return;
-	}
-	int num = 1;
-	if(argc >= 4) {
-		num = atoi(argv[3]);
-	}
-	if(num || strcmp(argv[3], "0")) {
-		defformatstring(_cmd)("bardrink %d %d %s %d", botcn, cn, argv[2], num);
-		if(identexists("bardrink")) execute(_cmd);
 	}
 }
 void histring(char *s) {
