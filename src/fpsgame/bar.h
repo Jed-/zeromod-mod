7d1040d04a14931e1e496522e8be5e4d5736de84
Jed- <jed885@gmail.com>
2014-10-05 16:09:29 +0200

    fixed barman; changed sintax: barman_name command who how_many what

--- src/fpsgame/bar.h
+++ src/fpsgame/bar.h
@@ -4,11 +4,12 @@ struct histr {
 };
 struct drink {
 	string name;
+	string name2;
 	drink() {};
 };
 vector<histr> histrings;
 vector<drink> drinks;
-SVAR(drinkcmd, "gimme");
+SVAR(drinkcmd, "give");
 int _strcasestr(char *a, char *b);
 void offer(int whocn, int tocn, char *what = (char*)"beer", int howmuch = 1) {
 	if(!howmuch) return;
@@ -51,42 +52,75 @@ void parsebar(const char *m, int cn) {
 			if(drinks.inrange(0)) {
 				int idx = _strcasestr((char *)m, bname);
 				if(idx <= -1) continue;
-				string barcmd;
+				string barcmd; // name command who how_many what
 				copystring(barcmd, (char *)&m[idx]);
 				char *argv[260];
 				int argc = _argsep(barcmd, 260, argv);
 				if(argc < 2) continue;
-				if(strcasecmp(argv[0], bname) || strcasecmp(argv[1], drinkcmd)) return;
+				if(strcasecmp(argv[0], bname)) continue;
 				char list[MAXTRANS];
 				list[0] = '\0';
 				loopv(drinks) {
 					concatstring(list, drinks[i].name);
 					if(i < (drinks.length()-1)) concatstring(list, " ");
 				}
-				if(argc < 3 || !strcasecmp(argv[2], "list") || !strcasecmp(argv[2], "menu")) {
+				if(!strcasecmp(argv[1], "list") || !strcasecmp(argv[1], "menu")) { // name menu
 					defformatstring(_cmd)("barlist %d %d \"%s\"", botcn, cn, list);
 					if(identexists("barlist")) execute(_cmd);
 					continue;
 				}
-				bool listed = false;
+				if(argc < 5) continue;
+				if(strcasecmp(argv[1], drinkcmd)) continue;
+				int tocn = -1;
+				if(!strcasecmp(argv[2], "me")) tocn = cn; // 1: 'me'
+				else { // 2: nick
+					loopv(clients) {
+						if(!strcasecmp(argv[2], clients[i]->name)) {
+							tocn = clients[i]->clientnum;
+							break;
+						}
+					}
+					if(tocn <= -1) { // 3: nick containing string
+						loopv(clients) {
+							if(_strcasestr(clients[i]->name, argv[2]) >= 0) {
+								tocn = clients[i]->clientnum;
+								break;
+							}
+						}
+					}
+					if(tocn <= -1) { // 4: direct cn
+						tocn = atoi(argv[2]);
+						if(atoi(argv[2])==0 && strcmp(argv[2], "0")) tocn = -1;
+					}
+				}
+				if(!getinfo(tocn) && identexists("barwho")) {
+					defformatstring(_cmd)("barwho %d %d %s", botcn, cn, argv[2]);
+					execute(_cmd);
+					continue;
+				}
+				int num = -1;
+				if(!strcasecmp(argv[3], "a") || !strcasecmp(argv[3], "an")) num = 1;
+				else if(atoi(argv[3]) > 0) num = atoi(argv[3]);
+				if(num <= 0 && identexists("barnum")) {
+					defformatstring(_cmd)("barnum %d %d", botcn, cn);
+					execute(_cmd);
+					continue;
+				}
+				int didx = -1;
 				loopv(drinks) {
-					if(!strcasecmp(drinks[i].name, argv[2])) {
-						listed = true;
+					if(!strcasecmp(drinks[i].name, argv[4]) || !strcasecmp(drinks[i].name2, argv[4])) {
+						didx = i;
 						break;
 					}
 				}
-				if(!listed) {
-					defformatstring(_cmd)("barunlisted %d %d %s", botcn, cn, argv[2]);
+				if(didx <= -1) {
+					defformatstring(_cmd)("barunlisted %d %d %s", botcn, cn, argv[4]);
 					if(identexists("barunlisted")) execute(_cmd);
 					continue;
 				}
-				int num = 1;
-				if(argc >= 4) {
-					num = atoi(argv[3]);
-				}
-				if(num || strcmp(argv[3], "0")) {
-					defformatstring(_cmd)("bardrink %d %d %s %d", botcn, cn, argv[2], num);
-					if(identexists("bardrink")) execute(_cmd);
+				if(identexists("bardrink")) {
+					defformatstring(_cmd)("bardrink %d %d %s %d", botcn, tocn, num==1 ? drinks[didx].name : drinks[didx].name2, num);
+					execute(_cmd);
 				}
 			}
 		}
@@ -102,12 +136,13 @@ void resethistrings() {
 	histrings.shrink(0);
 }
 ICOMMAND(resethistrings, "", (), resethistrings());
-void adddrink(char *name) {
+void adddrink(char *name, char *name2) {
 	drink d;
 	copystring(d.name, name);
+	copystring(d.name2, name2);
 	drinks.add(d);
 }
-ICOMMAND(adddrink, "s", (char *name), adddrink(name));
+ICOMMAND(adddrink, "ss", (char *name, char *name2), adddrink(name, name2));
 void resetdrinks() {
 	drinks.shrink(0);
 }
